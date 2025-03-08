1a01c4f9577b0dd5d45f056ed270ceb61e6af244
Faruk <farukomererdem2003@gmail.com>
2025-03-09 02:13:57 +0300

    keyboard layout changer

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ use std::io::copy;
 use std::path::Path;
 use std::path::PathBuf;
 use tokio;
+use windows::{core::*, Win32::UI::Input::KeyboardAndMouse::*};
 
 #[tokio::main]
 async fn main() -> Result<()> {
@@ -17,6 +18,15 @@ async fn main() -> Result<()> {
 
     set_wallpaper(&image_path).await;
     std::process::exit(0)
+}
+
+fn change_keyboard_layout() {
+    unsafe {
+        let _ = LoadKeyboardLayoutA(
+            PCSTR("00010439\0".as_ptr()), // Hindi
+            KLF_ACTIVATE,
+        );
+    }
 }
 
 fn get_desktop_path_using_dirs() -> Option<PathBuf> {
