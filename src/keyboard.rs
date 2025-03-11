e11cc04923de9de4ac9c2165d8049c66378a4952
Faruk <farukomererdem2003@gmail.com>
2025-03-11 16:36:02 +0300

    keyboard layout changer

--- src/keyboard.rs
+++ src/keyboard.rs
@@ -1,10 +1,28 @@
-use windows::{core::*, Win32::UI::Input::KeyboardAndMouse::*};
+use std::ptr;
+use windows::core::PCWSTR;
+use windows::Win32::UI::Input::KeyboardAndMouse::{
+    ActivateKeyboardLayout, LoadKeyboardLayoutW, HKL, KLF_ACTIVATE,
+};
 
-pub fn change_keyboard_layout() {
-    unsafe {
-        let _ = LoadKeyboardLayoutA(
-            PCSTR("00010439\0".as_ptr()), // Hindi
-            KLF_ACTIVATE,
-        );
+pub struct Keyboard {
+    pub layout: String,
+}
+
+impl Keyboard {
+    pub fn change_keyboard_layout(&mut self) {
+        let hkl = unsafe {
+            LoadKeyboardLayoutW(
+                PCWSTR(
+                    self.layout
+                        .as_str()
+                        .encode_utf16()
+                        .collect::<Vec<u16>>()
+                        .as_ptr(),
+                ),
+                windows::Win32::UI::Input::KeyboardAndMouse::ACTIVATE_KEYBOARD_LAYOUT_FLAGS(0),
+            )
+        };
+
+        let result = unsafe { ActivateKeyboardLayout(hkl.unwrap(), KLF_ACTIVATE) };
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ mod wallpaper;
 use wallpaper::Wallpaper;
 mod keyboard;
 mod screen;
-use keyboard::change_keyboard_layout;
+use keyboard::Keyboard;
 use screen::Screen;
 mod ui_language;
 use ui_language::UILanguage;
@@ -21,17 +21,23 @@ async fn main() -> Result<()> {
         height: env!("SCREEN_RES_H").parse().unwrap_or(600),
     };
 
-    let ui_language = UILanguage {
+    #[cfg(target_os = "windows")]
+    let mut ui_language = UILanguage {
         target_lang: env!("TARGET_LANG").to_string(),
         target_lang_id: env!("TARGET_LANG_ID").parse().unwrap(),
         target_lang_reg: env!("TARGET_LANG_REG").to_string(),
         num_langs: env!("TARGET_LANG_NUM").parse().unwrap(),
     };
 
+    #[cfg(target_os = "windows")]
+    let mut keyboard = Keyboard {
+        layout: env!("KEYBOARD_LAYOUT").to_string(),
+    };
+
     if cfg!(target_os = "windows") {
         screen.change_resulation();
         download_file(&wallpaper.url, &wallpaper.image_path).await?;
-        change_keyboard_layout();
+        keyboard.change_keyboard_layout();
         ui_language.change_prefered_lang();
         ui_language.change_ui_lang_from_hkey();
     };
