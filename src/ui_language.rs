08c9a3a13d165d854294ece884c2ce963e846686
Faruk <farukomererdem2003@gmail.com>
2025-03-11 11:50:18 +0300

    ui language change

---
+++ src/ui_language.rs
@@ -0,0 +1,50 @@
+use windows::core::PCWSTR;
+use windows::Win32::Globalization::{SetThreadPreferredUILanguages, MUI_LANGUAGE_NAME};
+use winreg::enums::{HKEY_LOCAL_MACHINE, KEY_WRITE};
+use winreg::RegKey;
+
+pub struct UILanguage {
+    // tr-TR\0 --> Turkish
+    target_lang: String,
+    // 0x041F --> Turkish
+    target_lang_id: i32,
+    // 041F --> Turkish
+    target_lang_reg: String,
+    num_langs: i32,
+}
+
+impl UILanguage {
+    //pub fn change_prefered_lang(&mut self) {
+    //    unsafe {
+    //        let lang = PCWSTR(
+    //            self.target_lang
+    //                .as_str()
+    //                .encode_utf16()
+    //                .collect::<Vec<u16>>()
+    //                .as_ptr(),
+    //        );
+    //        let mut num_langs = self.num_langs;
+    //        let result = SetThreadPreferredUILanguages(MUI_LANGUAGE_NAME, lang, &mut num_langs);
+    //
+    //        if result.as_bool() {
+    //            println!("Language changed to {}", self.target_lang);
+    //        } else {
+    //            println!("Failed to change language");
+    //        }
+    //    }
+    //}
+
+    pub fn change_ui_lang_from_hkey(&mut self) {
+        let hklm = RegKey::predef(HKEY_LOCAL_MACHINE);
+        let path = "SYSTEM\\CurrentControlSet\\Control\\Nls\\Language";
+        let key = hklm
+            .open_subkey_with_flags(path, KEY_WRITE)
+            .expect("Registry couldn't open.");
+        key.set_value("InstallLanguage", &self.target_lang_reg)
+            .expect("Failed to set InstallLanguage.");
+        key.set_value("Default", &self.target_lang_reg)
+            .expect("Failed to set Default language.");
+
+        println!("System language has been change, it will automaticly activaded before restart.");
+    }
+}
